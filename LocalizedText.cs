e95269b3eff84b89fb80e0e7d81a70220f8804a4
Donut Studio <98602568+Donut-Studio@users.noreply.github.com>
2022-03-05 08:48:59 +0100

    v1.1.0

---
+++ LocalizedText.cs
@@ -0,0 +1,46 @@
+﻿/*
+  Localization system - Extention for Unity to enable localization in your game.
+  Created by Donut Studio, March 05, 2022.
+  Released into the public domain.
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace DonutStudio.Utilities.LocalizationSystem
+{
+    [RequireComponent(typeof(Text))]
+    public class LocalizedText : MonoBehaviour
+    {
+        [SerializeField()]
+        private LocalizedValue key;
+        [SerializeField()]
+        private bool refreshOnChange = true;
+
+        private void Awake()
+        {
+            RefreshText();
+        }
+        private void OnEnable()
+        {
+            if (refreshOnChange)
+                LocalizationSystem.onRefresh += OnRefresh;
+        }
+        private void OnDisable()
+        {
+            if (refreshOnChange)
+                LocalizationSystem.onRefresh -= OnRefresh;
+        }
+
+        private void OnRefresh(object sender, EventArgs e)
+        {
+            RefreshText();
+        }
+        private void RefreshText()
+        {
+            if (LocalizationSystem.GetLocalizedValue(key.key, out string value))
+                GetComponent<Text>().text = value;
+        }
+    }
+}
